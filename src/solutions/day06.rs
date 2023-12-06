60bee9ddd1cc58b18b8d3adc03aa2a423414386e
Simon Sawert <simon@sawert.se>
2023-12-06 23:18:11 +0100

    Add the none brute-force way

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -8,45 +8,24 @@ pub fn solve() {
 }
 
 fn part_one(input: Vec<String>) -> i64 {
-    win_product(input.as_ref(), false)
-}
-
-fn part_two(input: Vec<String>) -> i64 {
-    win_product(input.as_ref(), true)
-}
-
-fn win_product(input: &[String], single_digit: bool) -> i64 {
-    let (times, distances) = times_and_distances(input, single_digit);
+    let (times, distances) = times_and_distances(input.as_ref(), false);
 
     times
-        .iter()
-        .enumerate()
-        .map(|(i, time)| {
-            let mut wins = 0;
-            let to_beat = distances[i];
-
-            let mut previous = 0;
-            for n in 0..*time {
-                let speed = n;
-                let time_for_speed = time - n;
-                let distance_travelled = speed * time_for_speed;
-
-                if distance_travelled > to_beat {
-                    wins += 1;
-                } else if distance_travelled < previous {
-                    // We've peaked and don't have a winning distance so abort.
-                    break;
-                }
-
-                previous = distance_travelled;
-            }
-
-            wins
-        })
+        .into_iter()
+        .zip(distances)
+        .map(find_possible_lengths)
         .product()
 }
 
-fn times_and_distances(input: &[String], single_digit: bool) -> (Vec<i64>, Vec<i64>) {
+fn part_two(input: Vec<String>) -> i64 {
+    let (times, distances) = times_and_distances(input.as_ref(), true);
+    let time = *times.first().unwrap();
+    let distance_to_beat = *distances.first().unwrap();
+
+    find_possible_lengths((time, distance_to_beat))
+}
+
+fn times_and_distances(input: &[String], single_digit: bool) -> (Vec<f64>, Vec<f64>) {
     let replace_space = if single_digit { "" } else { " " };
     let l1 = input
         .first()
@@ -62,15 +41,32 @@ fn times_and_distances(input: &[String], single_digit: bool) -> (Vec<i64>, Vec<i
 
     let times = l1
         .split_whitespace()
-        .map(|t| t.parse::<i64>().unwrap())
+        .map(|t| t.parse::<f64>().unwrap())
         .collect::<Vec<_>>();
 
     let distances = l2
         .split_whitespace()
-        .map(|t| t.parse::<i64>().unwrap())
+        .map(|t| t.parse::<f64>().unwrap())
         .collect::<Vec<_>>();
 
     (times, distances)
+}
+
+fn find_possible_lengths((race_duration, distance_to_beat): (f64, f64)) -> i64 {
+    let a = 1f64;
+    let b = -race_duration;
+    let c = distance_to_beat;
+
+    let discriminant = b.powf(2.) - 4. * a * c;
+    if discriminant < 0. {
+        return 0;
+    }
+
+    let sqrt_discriminant = discriminant.sqrt();
+    let root1 = (-b + sqrt_discriminant) / (2. * a);
+    let root2 = (-b - sqrt_discriminant) / (2. * a);
+
+    (root1.ceil() - root2.floor()) as i64 - 1
 }
 
 #[cfg(test)]
